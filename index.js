d6ab4b36ffa49947ba8b870fe323461bbefabb56
Yoqutjon Kuchimov <h98ghgy@gmail.com>
2026-03-10 16:33:46 +0500

    js 1-lesson

---
+++ index.js
@@ -0,0 +1,16 @@
+var name = "Zge"
+let year = 14
+const win = 1n
+var lose = null
+let best;
+const student = true
+let number = Symbol("student");
+
+
+console.log(name)
+console.log(year)
+console.log(win)
+console.log(lose)
+console.log(best)
+console.log(student)
+console.log(number)
