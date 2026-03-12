1523d09a8d1ac35e208d4125d13e0dd2f88ce543
Yoqutjon Kuchimov <h98ghgy@gmail.com>
2026-03-12 13:42:01 +0500

    js 2-lesson

--- index.js
+++ index.js
@@ -1,16 +1,84 @@
-var name = "Zge"
-let year = 14
-const win = 1n
-var lose = null
-let best;
-const student = true
-let number = Symbol("student");
+//1-masala
+// let a = "Yoqut__dee"
+// let b = 13
+// console.log(a+ " yoshim " +b)
 
+//2-masala
+// let pricing = 5000
+// let soni = 3
+// let all = pricing * soni
+// console.log(all)
 
-console.log(name)
-console.log(year)
-console.log(win)
-console.log(lose)
-console.log(best)
-console.log(student)
-console.log(number)
+//3-masala
+// let a = 10
+// let b = 14
+// console.log(a, b)
+
+//4-masala
+// const PI = 3.14
+// const PI2 = 7
+// const all = PI * PI2 * PI2
+// console.log(all)
+
+//5-masala
+// let firstname = "Ali"
+// let lastname = "Valiyev"
+// let age = 20
+// console.log("Ismim :" + firstname + " " + lastname + " yoshim: " + age )
+
+//6-masala
+// let a = 20
+// let b = 6
+// let jami = a + b - a * b / a
+// console.log(jami)
+
+//7-masala
+// let narxi = 12000
+// let chegirma = 15
+// let jami = narxi * chegirma / 100
+// let narxi2 = narxi - jami
+// console.log(narxi2)
+
+//8
+// let a = 9
+// let b = a ** 2
+// console.log(b)
+
+//9
+// let a = 42
+// let b = 5
+// let jami = a % b
+// console.log(`qoldiq : ${jami}` )
+
+// //10
+// let ball = 100
+// let coin = 20
+// let jami = ball += coin -= ball *= coin /= ball
+// console.log(jami)
+
+//11
+// let balans = 50000
+// balans += 10000
+// console.log(balans)
+// balans += 10000
+// console.log(balans)
+// balans += 10000
+// console.log(balans)
+
+//12
+// let a = 10
+// let b = 20
+// let jami = a === b
+// console.log(jami)
+
+//13
+// let x = 5
+// let y = "5"
+// let jami = x === y
+// console.log(jami)
+
+//14
+// let password = "abc123"
+// let type = "abc13"
+// let tekshiruv = password === type
+// console.log(tekshiruv)
