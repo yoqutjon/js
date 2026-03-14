61cf3ee304da2c469ac6a41bc3e553fac1e20ff7
Yoqutjon Kuchimov <h98ghgy@gmail.com>
2026-03-14 12:54:33 +0500

    js

--- index.js
+++ index.js
@@ -1,84 +1,198 @@
-//1-masala
-// let a = "Yoqut__dee"
-// let b = 13
-// console.log(a+ " yoshim " +b)
+// 1-masala
+//
+// let a = 14
+// let b = 2026
+// let c = b - a
+// console.log(c)
 
 //2-masala
-// let pricing = 5000
-// let soni = 3
-// let all = pricing * soni
-// console.log(all)
+// let yoshi = 14
+// let kun = 365
+// let all_days = yoshi * kun
+// let hours = 24
+// let all_hours = all_days * hours
+// let minutes = 60
+// let all_minutes = all_hours * minutes
+// console.log(yoshi, all_days, all_hours, all_minutes)
 
 //3-masala
-// let a = 10
-// let b = 14
-// console.log(a, b)
+// let a = 8
+// let b = 3
+// let jami = a + b - a * b / a % b
+// console.log(jami)
 
 //4-masala
-// const PI = 3.14
-// const PI2 = 7
-// const all = PI * PI2 * PI2
-// console.log(all)
+// let name = "Yoqutjon"
+// let surnameName = "Kuchimov"
+// console.log(`${surnameName} ${name}`)
 
 //5-masala
-// let firstname = "Ali"
-// let lastname = "Valiyev"
-// let age = 20
-// console.log("Ismim :" + firstname + " " + lastname + " yoshim: " + age )
+// let dollars_price = 12400
+// let dollar = 765
+// let all_dollars = dollar * dollars_price
+// console.log(all_dollars)
 
 //6-masala
-// let a = 20
-// let b = 6
-// let jami = a + b - a * b / a
-// console.log(jami)
-
+// let age = prompt("yoshingni kirit");
+// console.log(`senn hozir ${age} yoshdasan`);
 //7-masala
-// let narxi = 12000
-// let chegirma = 15
-// let jami = narxi * chegirma / 100
-// let narxi2 = narxi - jami
-// console.log(narxi2)
+//
+// let yoshi = prompt("Salom yoshingni kirit")
+//
+// if (yoshi < 18) {
+//     console.log("Siz hali voyaga yetmagansiz")
+// } else {
+//     console.log("Siz voyaga yetgansiz")
+// }
 
-//8
-// let a = 9
-// let b = a ** 2
-// console.log(b)
+//8-masala
 
-//9
-// let a = 42
-// let b = 5
-// let jami = a % b
-// console.log(`qoldiq : ${jami}` )
+// let musbat = prompt("krch bitta raqam kiriting")
+// if (musbat < 0) {
+//    console.log("bu son manfiy")
+// } else if (musbat > 0) {
+//     console.log("bu son musbat")
+// } else {
+//     console.log("bu son 0 ga teng")
+// }
 
-// //10
-// let ball = 100
-// let coin = 20
-// let jami = ball += coin -= ball *= coin /= ball
-// console.log(jami)
+//9-masala
 
-//11
-// let balans = 50000
-// balans += 10000
-// console.log(balans)
-// balans += 10000
-// console.log(balans)
-// balans += 10000
-// console.log(balans)
+// let password = prompt("Enter your password");
+// if (password === "manfaat123") {
+//     console.log("hush kelibsiz ")
+// } else {
+//     console.log("parol vopshe notug'ri")
+// }
 
-//12
-// let a = 10
-// let b = 20
-// let jami = a === b
-// console.log(jami)
+//10-masala
 
-//13
-// let x = 5
-// let y = "5"
-// let jami = x === y
-// console.log(jami)
+// let baho = prompt("Bahongizni kiriting:")
+//
+// if (baho > 90) {
+//     console.log("A'lo!")
+// } else if (baho >= 70 && baho <= 89) {
+//     console.log("Yaxshi")
+// } else if (baho >= 50 && baho <= 69) {
+//     console.log("Qoniqarli")
+// } else {
+//     console.log("Yomon")
+// }
 
-//14
-// let password = "abc123"
-// let type = "abc13"
-// let tekshiruv = password === type
-// console.log(tekshiruv)
+//11-masala
+
+// let kun = prompt("hafta kunini kiriting:")
+// switch (kun) {
+//     case "dushanba":
+//         console.log("ish kuni")
+//         break;
+//
+//     case "seshanba":
+//         console.log("ish kuni")
+//         break;
+//
+//     case "chorshanba":
+//         console.log("ish kuni")
+//         break;
+//
+//     case "payshanba":
+//         console.log("ish kuni")
+//         break;
+//
+//     case "juma":
+//         console.log("ish kuni")
+//         break;
+//
+//     case "shanba":
+//         console.log("dam olish kuni")
+//         break;
+//
+//     case "yakshanba":
+//         console.log("dam olish kuni")
+//         break;
+//
+//     default:
+// }
+
+//12-masala
+
+// let harorat = prompt("Haroratni kiriting:")
+//
+// if (harorat > 30) {
+//     console.log("Juda issiq ko‘proq suv iching nu lekin uzbekchilik")
+// } else if (harorat >= 15 && harorat <= 30) {
+//     console.log("ob havo yaxshi")
+// } else if (harorat >= 0 && harorat < 15) {
+//     console.log(" iliq kiying.")
+// } else {
+//     console.log("Sovuq  qalin kiying!")
+// }
+
+//13-masala
+//
+// let son1 = 12
+// let son2 = 45
+// let son3 = 30
+//
+// if (son1 >= son2 && son1 >= son3) {
+//     console.log("Eng katta son:", son1)
+// } else if (son2 >= son1 && son2 >= son3) {
+//     console.log("Eng katta son:", son2)
+// } else {
+//     console.log("Eng katta son:", son3)
+// }
+
+//14-masala
+
+// idk
+
+//15
+
+// let raqam = prompt("raqamni kirit")
+// if (raqam === "+998") {
+//     console.log("bu raqam uzbni raqami")
+// } else{
+//     console.log("raqam uzbni raqami emas")
+// }
+
+//16
+// let a = 3;
+// let b = 7;
+//
+// for (let i = a + 1; i < b; i++) {
+//     console.log(i);
+// }
+
+//17
+//
+// let price = 20000;
+//
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i, "kg", price * i);
+// }
+
+//18
+// let price = 20000;
+//
+// for (let i = 1; i <= 10; i++) {
+//     let kg = i / 10;
+//     console.log(kg, "kg  narxi ", price * kg);
+// }
+
+//19
+// let a = 3;
+// let b = 6;
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//     sum += i;
+// }
+// console.log("yig'indisi:", sum);
+
+//20
+// let a = 3;
+// let b = 6;
+// let produkta = 1;
+// for (let i = a; i <= b; i++) {
+//     produkta *= i;
+// }
+// console.log("kopaytmasi", produkta);
